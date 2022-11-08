6c3b1c4a731c1764c57184f04b62481c6cf03de7
Dmitry Filippov <demodima97@mail.ru>
2022-11-09 01:25:10 +0300

    done

--- src/components/TextItem/TextItem.js
+++ src/components/TextItem/TextItem.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Text, Transformer } from "react-konva";
 
-// type TextItemProps = {
-//   text: string;
-//   isSelected: boolean;
-//   fontSize: number;
-//   setFontSize: any;
-// };
-
 const TextItem = ({ isSelected, onSelect, text, x, y }) => {
   const shapeRef = React.useRef();
   const trRef = React.useRef();
@@ -20,29 +13,29 @@ const TextItem = ({ isSelected, onSelect, text, x, y }) => {
   }, [isSelected]);
 
   return (
-    <React.Fragment>
-      <Text
-        onClick={onSelect}
-        onTap={onSelect}
-        ref={shapeRef}
-        fontSize={38}
-        draggable
-        text={text}
-        x={x}
-        y={y}
-      />
-      {isSelected && (
-        <Transformer
-          ref={trRef}
-          boundBoxFunc={(oldBox, newBox) => {
-            if (newBox.width < 5 || newBox.height < 5) {
-              return oldBox;
-            }
-            return newBox;
-          }}
+      <React.Fragment>
+        <Text
+          onClick={onSelect}
+          onTap={onSelect}
+          ref={shapeRef}
+          fontSize={38}
+          draggable
+          text={text}
+          x={x}
+          y={y}
         />
-      )}
-    </React.Fragment>
+        {isSelected && (
+          <Transformer
+            ref={trRef}
+            boundBoxFunc={(oldBox, newBox) => {
+              if (newBox.width < 5 || newBox.height < 5) {
+                return oldBox;
+              }
+              return newBox;
+            }}
+          />
+        )}
+      </React.Fragment>
   );
 };
 
